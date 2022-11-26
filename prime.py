df17989fd30ef1f16970dd6991ab07624c99850a
Akshaya152 <115154677+Akshaya152@users.noreply.github.com>
2022-11-26 12:44:53 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value"))
+count=0
+for i in range(2,(a//2)+1))
+ if(a%i==0):
+  count+=1
+if (count==0):
+  print("it is a prime number")
+else:
+  print("it is not a prime number")
+ 
